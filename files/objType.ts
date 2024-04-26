9b990d9a6d9291858bab8892d1869d8adfd2d4e4
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-04-26 19:40:44 -0300

    mjs files added

--- files/objType.ts
+++
@@ -1,7 +0,0 @@
-export default function objType (obj, type) {
-    if (Object.prototype.toString.call(obj).toLowerCase() === `[object ${type}]`) {
-        return true;
-    } else {
-        return false;
-    }
-};
